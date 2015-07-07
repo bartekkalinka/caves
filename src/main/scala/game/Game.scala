c83a185259b378bff1daea24e7cdc7c10c36a4ab
Bartek Kalinka <bartkali@hotmail.com>
2015-07-07 22:50:35 +0200

    Putting actors loop together vol. 2

--- src/main/scala/game/Game.scala
+++ src/main/scala/game/Game.scala
@@ -1,41 +1,73 @@
 package game
 
 import akka.actor._
+import scala.concurrent.duration._
 
-case class Step(step: Int)
+case object Tick
+case class StateData(prefix: String, letter: String)
+case class UserInput(dir: Option[String])
+case class StepData(state: StateData, input: UserInput)
+case class StateMod(addToPrefix: Int)
 
 object StepActor {
   def props() = Props(classOf[StepActor])
 }
 class StepActor extends Actor with ActorLogging {
-  val words = Array("g", "w", "t", "e", "h")
-  var prefix = ""
-
   def receive = {
-    case Step(step) => {
-      context.actorSelection("../websocket") ! Push(prefix + words(step))
-    }
-
-    case UserInput("right") => {
-      prefix += "X"
-    }
-
-    case UserInput("left") => {
-      prefix = if(prefix.length > 0) prefix.substring(1) else prefix
+    case StepData(state, input) => {
+      val addToPrefix = input.dir match {
+          case Some ("right") => 1
+          case Some("left") => -1
+          case None => 0
+      }
     }
   }
+}
+
+object Input {
+  def props() = Props(classOf[State])
+}
+class Input extends Actor with ActorLogging {
+  var userInput: UserInput = UserInput(None)
+
+  def receive = {
+    case ui: UserInput => userInput = ui
+    case sd: StateData => {
+      context.actorSelection("../step") ! StepData(sd, userInput)
+    }
+  }
+}
+
+object State {
+  def props() = Props(classOf[State])
+}
+class State extends Actor with ActorLogging {
+  val stream = Stream.iterate(0)(x => if(x == 4) 0 else x + 1)
+  val words = Array("g", "w", "t", "e", "h")
+  var prefix = ""
+  var index = 0
+
+  override def preStart(): Unit = { context.system.scheduler.scheduleOnce(1 seconds,self, Tick) }
+
+  def receive = {
+    case StateMod(addToPrefix) => {
+      //apply modifications
+      //TODO separate into a method
+      val prefixLength = Math.max(prefix.length + addToPrefix, 0)
+      prefix = List.tabulate(prefixLength)(x => " ").mkString("")
+      //TODO change index
+
+      //broadcast state to client
+      //TODO separate into a method
+      context.actorSelection("../websocket") ! Push(prefix + words(index))
+    }
+
+    case Tick => context.actorSelection("../input") ! StateData(prefix, words(index))
+  }
+
 }
 
 //TODO possible/planned actors:
 //Clock (currently Game object)
 //Step
 //State (currenty in Step)
-
-object Game {
-  def play(stepActor: ActorRef) = {
-    for(index <- Stream.iterate(0)(x => if(x == 4) 0 else x + 1)) {
-      stepActor ! Step(index)
-      Thread.sleep(50)
-    }
-  }
-}
