373e02742b32a0debfd9b031cedd03e06236d652
Bartek Kalinka <bartkali@hotmail.com>
2016-01-12 16:18:10 +0100

    face direction in player move

--- src/main/scala/game/Game.scala
+++ src/main/scala/game/Game.scala
@@ -10,8 +10,15 @@ case class StateData(player: Player, moveStepInPixels: Int)
 case class UserInput(dir: String)
 case class StepData(state: StateData, input: Option[UserInput])
 
+sealed trait FaceDirection
+object FaceDirection {
+  object Straight extends FaceDirection
+  object Right extends FaceDirection
+  object Left extends FaceDirection
+}
+
 sealed trait InputDrivenMod
-case class PlayerMove(xMod: Int, yMod: Int) extends InputDrivenMod
+case class PlayerMove(xMod: Int, yMod: Int, faceDirection: FaceDirection = FaceDirection.Straight) extends InputDrivenMod
 case class Zoom(in: Boolean) extends InputDrivenMod
 sealed trait OtherMod
 case class NewOther(x: Int) extends OtherMod
@@ -22,9 +29,9 @@ case class StateMod(inputDrivenModOpt: Option[InputDrivenMod])
 object Step {
   def step(data: StepData): StateMod = {
     val inputDrivenModOpt = data.input.map  {
-      case UserInput("right") => PlayerMove(data.state.moveStepInPixels, 0)
+      case UserInput("right") => PlayerMove(data.state.moveStepInPixels, 0, FaceDirection.Right)
       case UserInput("up") => PlayerMove(0, -data.state.moveStepInPixels)
-      case UserInput("left") => PlayerMove(-data.state.moveStepInPixels, 0)
+      case UserInput("left") => PlayerMove(-data.state.moveStepInPixels, 0, FaceDirection.Left)
       case UserInput("down") => PlayerMove(0, data.state.moveStepInPixels)
       case UserInput("zoomin") => Zoom(true)
       case UserInput("zoomout") => Zoom(false)
@@ -49,7 +56,7 @@ case class State(player: Player, score: Int, tilePixels: Int)
 {
   def applyMod(mod: StateMod): State = {
     mod.inputDrivenModOpt.map {
-      case PlayerMove(xMod, yMod) => copy(player = player.copy(x = player.x + xMod, y = player.y + yMod))
+      case PlayerMove(xMod, yMod, _) => copy(player = player.copy(x = player.x + xMod, y = player.y + yMod))
       case Zoom(in) => copy(tilePixels =
         if(in) math.floor(tilePixels * Const.zoomFactor).toInt
         else math.floor(tilePixels / Const.zoomFactor).toInt)
