c6693d074b58bee4407568eab81a7bd7a6899c01
Bartek Kalinka <bartkali@hotmail.com>
2016-01-26 11:42:02 +0100

    larger player

--- src/main/scala/game/Game.scala
+++ src/main/scala/game/Game.scala
@@ -68,7 +68,7 @@ object Const {
   private val targetNoiseDetail = 3
   private val baseMultiplier = Math.pow(2, Const.targetNoiseDetail).toInt
   val tilesPerShape = baseTilesPerShape * baseMultiplier
-  val initTilePixels = 64 / baseMultiplier
+  val initTilePixels = 96 / baseMultiplier
   val moveStepInTiles = 1
   val zoomFactor = Math.sqrt(1.5)
   val screenWidth = 768
