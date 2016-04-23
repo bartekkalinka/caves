92904b120f7f676428961d9c8c2dacbf087c1227
Bartek Kalinka <bartkali@hotmail.com>
2016-04-23 16:53:00 +0200

    players coords -> global state api

--- src/main/scala/game/state/GlobalState.scala
+++ src/main/scala/game/state/GlobalState.scala
@@ -16,4 +16,13 @@ object GlobalState {
     }
     playerCounter.updateAndGet(longUnaryOperator)
   }
+
+  private val playersCoords = new scala.collection.concurrent.TrieMap[Long, (Int, Int)]
+
+  def putPosition(playerId: Long, coord: (Int, Int)) = playersCoords.put(playerId, coord)
+
+  def getPositionsWithinRectangle(upperLeft: (Int, Int), lowerRight: (Int, Int)) =
+    playersCoords.toSeq.filter { case (id, coord) =>
+      coord._1 >= upperLeft._1 && coord._2 >= upperLeft._2 && coord._1 < lowerRight._1 && coord._2 < lowerRight._2
+    }
 }
